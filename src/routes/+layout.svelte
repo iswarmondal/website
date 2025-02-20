55aef6f9f2ba6b3d740a908e84e1e6203b95754d
Iswar <73457704+iswarmondal@users.noreply.github.com>
2025-02-20 15:36:51 +0530

    Add Tailwind CSS and configure project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import type { Snippet } from 'svelte';
+
+	let { children }: { children: Snippet } = $props();
+</script>
+
+<main>
+	<Navbar />
+	{@render children()}
+</main>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,88 @@
+<div class="flex min-h-[95vh] flex-col items-start justify-center pl-10">
+	<div class="flex max-w-4xl flex-col items-start justify-center">
+		<div class="flex items-center gap-2">
+			<h1 class="text-3xl font-bold">Hello there</h1>
+		</div>
+		<hr class="my-4 w-full border-gray-700" />
+
+		<p class="text-left text-lg">
+			🎓 I'm a cat-loving full-stack developer, wandering around on the internet
+		</p>
+		<p class="text-left text-lg">👨‍💻 Passionate about Flutter, Sveltekit and Pokemon</p>
+
+		<h2 class="mt-8 text-2xl font-bold">My Experiences 👷‍♂</h2>
+		<hr class="my-4 w-full border-gray-700" />
+
+		<ul class="flex flex-col gap-2 text-left">
+			<li>
+				- I did an Internship in a media company <a
+					href="https://www.linkedin.com/company/futurex-global/mycompany/"
+					class="text-blue-400">Futurex Global</a
+				> as a front end developer
+			</li>
+			<li>
+				- I developed website for <a href="http://www.rampurhatgmch.edu.in/" class="text-blue-400"
+					>Rampurhat Govt. Medical College</a
+				> with PHP and MySQL
+			</li>
+			<li>
+				- I helped local businesses to go online during lockdown by developing e-commerce websites
+				for them (eg- <a href="https://dosarindustries.com/" class="text-blue-400"
+					>Dosar Industries</a
+				>)
+			</li>
+			<li>
+				- Worked as a jr. developer in <a href="https://www.bcmnext.com/" class="text-blue-400"
+					>BCM Next</a
+				>
+			</li>
+			<li>
+				- Developed a crypto wallet chrome extension for <a
+					href="https://netsepio.com/"
+					class="text-blue-400">NetSepio</a
+				> using Sveltekit
+			</li>
+			<li>
+				- Currently working on revamping traditional news classification system with <a
+					href="https://github.com/iwanalabs/"
+					class="text-blue-400">Iwana Labs</a
+				>
+			</li>
+		</ul>
+
+		<h2 class="mt-8 text-2xl font-bold">Ongoing Project 🚧</h2>
+		<hr class="my-4 w-full border-gray-700" />
+		<p class="text-left">
+			- I'm currently working on a self-classifying news app <a
+				href="https://news.iwanalabs.com/"
+				class="text-blue-400">news.iwanalabs.com</a
+			>
+		</p>
+
+		<div class="mt-8 flex flex-col items-start">
+			<div class="flex items-center gap-2">
+				<h2 class="text-2xl font-bold italic">Adventures are Epic</h2>
+				<a href="https://www.linkedin.com/in/iswarmondal/">
+					<img
+						src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+						class="h-6 w-6"
+						alt="LinkedIn"
+					/>
+				</a>
+				<a href="https://twitter.com/developeriswar">
+					<img
+						src="https://cdn-icons-png.flaticon.com/512/174/174876.png"
+						class="h-6 w-6"
+						alt="Twitter"
+					/>
+				</a>
+			</div>
+			<hr class="my-4 w-full border-gray-700" />
+			<img
+				src="https://github.com/iswarmondal/iswarmondal/assets/73457704/2d4f826e-1177-4ae1-9e46-16dfd519a259"
+				alt="adventure"
+				class="mt-4 max-w-xl"
+			/>
+		</div>
+	</div>
+</div>
